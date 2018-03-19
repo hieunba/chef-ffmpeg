57ce666cb280a41eb63031e212ed9ec08dfcb97a
Nghiem Ba Hieu <hi3unb@gmail.com>
2018-03-19 17:58:16 +0700

    Make foodcritic happy for FC064 and FC065

--- metadata.rb
+++ metadata.rb
@@ -13,10 +13,10 @@ supports 'ubuntu', '>= 16.04'
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
 # uploaded to a Supermarket.
 #
-# issues_url 'https://github.com/<insert_org_here>/ffmpeg/issues'
+issues_url 'https://github.com/hieunba/ffmpeg/issues'
 
 # The `source_url` points to the development repository for this cookbook.  A
 # `View Source` link will be displayed on this cookbook's page when uploaded to
 # a Supermarket.
 #
-# source_url 'https://github.com/<insert_org_here>/ffmpeg'
+source_url 'https://github.com/hieunba/ffmpeg'
